35a56aa2c40d34a826fec0f45c6799de5b363580
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2019-12-03 13:58:50 +0100

    Project Loadup

---
+++ javafx/src/main/java/_283095/javafx/App.java
@@ -0,0 +1,13 @@
+package _283095.javafx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
