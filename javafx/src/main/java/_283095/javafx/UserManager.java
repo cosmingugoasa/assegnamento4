f3837007dad2b155c5a5409822f0a9b47e215fbe
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2020-01-05 11:34:08 +0100

    Added UserManagerController

---
+++ javafx/src/main/java/_283095/javafx/UserManager.java
@@ -0,0 +1,22 @@
+package _283095.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class UserManager {
+
+    @FXML
+    private HBox hbAttivita;
+
+    @FXML
+    private HBox hbIscrizioni;
+
+    @FXML
+    private Button btnIscriviti;
+
+    @FXML
+    private Button btnDisiscriversi;
+
+}
+
