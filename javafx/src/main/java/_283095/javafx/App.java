30074ffe327f6118b54309223a5d4836f4178912
Mattia <locastromattia@hotmail.it>
2020-01-05 10:43:07 +0100

    Login and Connection

--- javafx/src/main/java/_283095/javafx/App.java
+++ javafx/src/main/java/_283095/javafx/App.java
@@ -1,13 +1,31 @@
 package _283095.javafx;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+  private static final String fileFxml = "Login.fxml";
+
+  @Override
+  public void start(Stage primaryStage) throws IOException
+  {
+    Parent root = FXMLLoader.load(getClass().getResource(fileFxml));
+    Scene scene = new Scene(root);
+
+    primaryStage.setTitle("Circolo Sportivo");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 }
---
+++ javafx/src/main/java/_283095/javafx/Login.java
@@ -0,0 +1,92 @@
+package _283095.javafx;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login
+{
+  private static final String DBURL = "mysql-loca.alwaysdata.net";
+  String dbName = "loca_circolosportivo";
+  private static final String LOGIN = "loca";
+  private static final String PASSWORD = "prova98";
+  String url = "jdbc:mysql://localhost:3306/";
+  String driver = "com.mysql.cj.jdbc.Driver";
+
+  @FXML
+  private Button btnLogin;
+
+  @FXML
+  private Button btnSignUp;
+
+  @FXML
+  private Label lbEmail;
+
+  @FXML
+  private Label lbPwd;
+
+  @FXML
+  private TextField tbEmail;
+
+  @FXML
+  private PasswordField tbPwd;
+
+  @FXML
+  private Label lbAction;
+
+  @FXML
+  void Login(ActionEvent event)
+  {
+    if (tbEmail.getText().contains("@") && !tbPwd.getText().isEmpty())
+      // lbAction.setText("Bottone Premuto");
+      Login("fa", "fe");
+    else
+      lbAction.setText("Inserire correttamente i parametri");
+  }
+
+  @FXML
+  void SignUp(ActionEvent event)
+  {
+
+  }
+
+  private void Login(final String email, final String pwd)
+  {
+    try
+    {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection con = DriverManager.getConnection(
+          "jdbc:mysql://mysql-loca.alwaysdata.net/loca_circolosportivo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+          LOGIN, PASSWORD);
+
+      if (con != null)
+      {
+        lbAction.setText("Connesso");
+      }
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("select * from PERSONA");
+      while (rs.next())
+      {
+        String lastName = rs.getString("name");
+        System.out.println(lastName + "\n");
+        
+      }
+      //con.close();
+    }
+    catch (SQLException | ClassNotFoundException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+}
