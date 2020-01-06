1c9056d29f3b00763d3b92fde850fc48c593ed66
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2020-01-06 11:16:22 +0100

    Added initialize function on UI

--- javafx/src/main/java/_283095/javafx/UserManager.java
+++ javafx/src/main/java/_283095/javafx/UserManager.java
@@ -1,22 +1,52 @@
 package _283095.javafx;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ListView;
 
-public class UserManager {
+public class UserManager
+{
 
-    @FXML
-    private HBox hbAttivita;
+  private static final String LOGIN = "loca";
+  private static final String PASSWORD = "prova98";
 
-    @FXML
-    private HBox hbIscrizioni;
+  @FXML
+  private Button btnIscriviti;
 
-    @FXML
-    private Button btnIscriviti;
+  @FXML
+  private Button btnDisiscriversi;
 
-    @FXML
-    private Button btnDisiscriversi;
+  @FXML
+  private ListView<String> lvAttivita;
+
+  @FXML
+  private ListView<String> lvIscrizioni;
+
+  @FXML
+  public void initialize() throws SQLException
+  {
+    // POPULATE LISTVIEWS
+    Connection con = DriverManager.getConnection(
+        "jdbc:mysql://mysql-loca.alwaysdata.net/loca_circolosportivo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+        LOGIN, PASSWORD);
+
+    if (con != null)
+    {
+      return;
+    }
+    
+    Statement stmt = con.createStatement();
+    ResultSet rs = stmt.executeQuery("select * from ATTIVITA");
+    while (rs.next())
+    {
+      lvAttivita.getItems().add(rs.toString());
+    }
+  }
 
 }
-
