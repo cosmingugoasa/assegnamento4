f76f1ce946a52b8b701432bbadfc74c0898d3976
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2020-01-07 16:43:13 +0100

    Added Graphics

--- javafx/src/main/java/_283095/javafx/AdminManager.java
+++ javafx/src/main/java/_283095/javafx/AdminManager.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class AdminManager {
 
@@ -29,6 +31,35 @@ public class AdminManager {
 
     @FXML
     private Button btnDeleteActivities;
+    
+    //ADD USER ELEMENTS
+    
+    @FXML
+    private TextField tfUserName;
+
+    @FXML
+    private TextField tfUserSurname;
+
+    @FXML
+    private TextField tfUserMail;
+
+    @FXML
+    private TextField tfUserPassword;
+
+    @FXML
+    private Button btnConfirm;
+    
+    //ADD ACTIVITIES ELEMENTS
+    
+    @FXML
+    private TextField tfActivityName;
+
+    @FXML
+    private CheckBox cbCourse;
+
+    @FXML
+    private CheckBox cbRace;
+
 
     @FXML
     public void initialize() throws SQLException, IOException
